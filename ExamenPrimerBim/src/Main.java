36b783d9021a0ffe5cb2a9c98e0970fb49ce08e5
AlejandroGutierrez01 <alejandro.gutierrez01@epn.edu.ec>
2024-06-14 13:01:10 -0500

    correccion de metodos en las clases, instanciacion de

--- ExamenPrimerBim/src/Main.java
+++ ExamenPrimerBim/src/Main.java
@@ -10,6 +10,11 @@ public class Main {
         VIP pasajeroVip4  = new VIP(null, null, null, null, 0, null, null, null, 0.0,null,false,false,false,false,null,null,0.0);
         VIP pasajeroVip5  = new VIP(null, null, null, null, 0, null, null, null, 0.0,null,false,false,false,false,null,null,0.0);
         VIP pasajeroVip6 = new VIP("VIP2024-123","10","Ale","051316132684",15,"065161","Quito","Guayaquil",2.1,"",true,true,true,false,"Comida","Cobija",20);
+        VIP pasajeroVip7 = new VIP("VIP2024-123","10","Ale","051316132684",15,"065161","Quito","Guayaquil",2.1,"",true,true,true,false,"Comida","Cobija",20);
+        VIP pasajeroVip8 = new VIP("VIP2024-123","10","Ale","051316132684",15,"065161","Quito","Guayaquil",2.1,"",true,true,true,false,"Comida","Cobija",20);
+        VIP pasajeroVip9 = new VIP("VIP2024-123","10","Ale","051316132684",15,"065161","Quito","Guayaquil",2.1,"",true,true,true,false,"Comida","Cobija",20);
+        VIP pasajeroVip10 = new VIP("VIP2024-123","10","Ale","051316132684",15,"065161","Quito","Guayaquil",2.1,"",true,true,true,false,"Comida","Cobija",20);
+
         Normal pasajeroNormal = new Normal("2024-123","","","",11,"","","",15.12,"",true,false,15.2);
         pasajeroNormal.mostrarTicket();
         System.out.println();
